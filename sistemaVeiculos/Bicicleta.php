506d0e89b715133ae71b047d3c14ecb75f99b240
Maria Clara Monteiro <mcmpacheco7@gmail.com>
2026-09-14 00:37:34 -0300

    feat: adiciona classe Bicicleta com método de aceleração e validações

---
+++ sistemaVeiculos/Bicicleta.php
@@ -0,0 +1,20 @@
+<?php
+namespace SistemaVeiculos;
+use sistemaVeiculos\Veiculo;
+require_once "Veiculo.php";
+
+class Bicicleta extends Veiculo{
+
+    public function acelerar(int $velocidade): void{ // com limite de velocidade 
+        $velocidadeAtual = $this->getVelocidade();
+        $velocidadeMaxima = $this->getVelocidadeMaxima();
+        if($velocidade < 0){
+            throw new \Exception("Velocidade inválida. Não pode ser negativa");
+        }
+
+        if($velocidadeAtual + $velocidade > $velocidadeMaxima){
+            throw new \Exception("Velocidade máxima excedida");
+        }
+        $this->setVelocidade($velocidadeAtual + $velocidade);
+    }
+}
